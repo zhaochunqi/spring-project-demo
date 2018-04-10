f82286533bf37f72384d4f6fb61ca984a4f3e605
zhaochunqi <git@zhaochunqi.com>
2018-04-10 23:48:53 +0800

    spring boot init
    This is generated by intellij idea

---
+++ src/main/java/com/zhaochunqi/spring/springprojectdemo/SpringProjectDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zhaochunqi.spring.springprojectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zhaochunqi/spring/springprojectdemo/SpringProjectDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhaochunqi.spring.springprojectdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringProjectDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
